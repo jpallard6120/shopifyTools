a3265b5e18fd0700cfb5853abdbdf7142d61180f
Jean-Philippe Allard <jp@jpallard.com>
2023-10-25 15:59:04 -0400

    Change to enable easier debugging

--- customerEventsListener/modules/customerEvents.js
+++ customerEventsListener/modules/customerEvents.js
@@ -1,21 +1,21 @@
 const customerEvents = () => {
-  // Listen to Shopify Customer Events and push data to dataLayer - jpallard.com
-    addEventListener('message', (event) => { 
+   addEventListener('message', (event) => { 
       if (JSON.stringify(event.data).includes("shopify_pixel_event")) {
         try {
           var json_data = JSON.parse(event.data.json)
         } catch (SyntaxError) {
           var json_data = {}
         }
-        events = ['view_item', 'view_item_list']
-        if (events.some(e => event.data.event_name.includes(e))) {
+        // let events = ['view_item', 'view_item_list']
+        // if (events.some(e => event.data.event_name.includes(e))) {
+        if (typeof(event.data.event_name) !== 'undefined') {
           window.dataLayer = window.dataLayer || [];
           window.dataLayer.push({
 	          event: event.data.event_name,
 	          ga4Data: json_data
           });
-          // console.log('Event name is: ', event.data.event_name)
-          // console.log('Data is: ', json_data)
+          console.log('Event name is: ', event.data.event_name)
+          console.log('Data is: ', json_data)
         }
     }
   });
