d1694b8eb00f2b7d97855cbfb79e9e0c10e09274
Jean-Philippe Allard <jp@jpallard.com>
2023-10-30 23:45:47 -0400

    Customer events v1 - working

---
+++ customerEventsPixels/ga4CheckoutEvents.js
@@ -0,0 +1,89 @@
+// Initialize gtag.js for subsequent events
+// This only works in a first party context, where the GA4 Client is sGTM is set to serve gtag.js
+var measurementID = 'G-LBN6XJ9NHK'
+var serverContainerURL = 'https://gtm.mailmasters.ca'
+// Remove debug_mode below for prod
+
+const script = document.createElement('script');
+script.setAttribute('src', `https://gtm.mailmasters.ca/gtag/js?id=${measurementID}`);
+script.setAttribute('async', '');
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('set', {
+'cookie_flags': 'SameSite=None;Secure'
+});
+gtag('js', new Date());
+gtag('config', measurementID, {
+  'send_page_view': false,
+  'debug_mode': true, // REMOVE THIS FOR PROD
+  'server_container_url': serverContainerURL
+});
+
+
+// Transforms Shopify items data schema to GA4 items schema
+function transformVariantToItem(variant, index = 0, quantity = 1) {
+    return {
+        item_id: variant.sku,
+        product_id: variant.product.id, // Not a default item level dimension. Need to add as a CD. 
+        variant_id: variant.id, // Not a default item level dimension. Need to add as a CD. The variant ID is broken on Ajax ATC events, and returns the line ID instead. 
+        item_name: variant.product.title,
+        affiliation: variant.product.vendor,
+        index: index,
+        item_brand: variant.product.vendor,
+        item_category: variant.product.type,
+        item_variant: variant.title,
+        price: variant.price.amount,
+        quantity: variant.quantity || quantity
+    };
+  }
+
+  function transformCheckoutData(checkoutData) {
+    console.log('Checkout data is: ', checkoutData)
+    const checkoutValue = checkoutData.totalPrice.amount
+    const checkoutCurrency = checkoutData.currencyCode
+    const checkoutLines = checkoutData.lineItems
+
+    let checkoutItems = []
+    checkoutLines.forEach((checkoutLine, index) => {
+        let checkoutItem = transformVariantToItem(checkoutLine.variant, index)
+        checkoutItem.quantity = checkoutLine.quantity
+        checkoutItems.push(checkoutItem);
+      });
+
+    console.log('checkoutItems are: ', checkoutItems)
+
+    return {
+    currency: checkoutCurrency,
+    value: checkoutValue,
+    items: checkoutItems
+    };
+    }
+
+// Start events tracking
+
+analytics.subscribe("checkout_started", async (event) => {
+    console.log('begin_checkout : ', event)
+    const transformedData = transformCheckoutData(event.data.checkout);
+    gtag('event', 'begin_checkout', transformedData);
+});
+
+
+analytics.subscribe("checkout_shipping_info_submitted", async (event) => {
+    console.log('add_shipping_info : ', event)
+    const transformedData = transformCheckoutData(event.data.checkout);
+    gtag('event', 'add_shipping_info', transformedData);
+    // Need to add user data
+    // Need to add shipping_method
+});
+
+
+analytics.subscribe("checkout_completed", async (event) => {
+    console.log('purchase : ', event)
+    const transformedData = transformCheckoutData(event.data.checkout);
+    gtag('event', 'purchase', transformedData);
+    // Need to add user data
+    // Need to add source data (to differentiate from webhook events)
+});
+
--- customerEventsPixels/ga4Events.js
+++ customerEventsPixels/ga4Events.js
@@ -1,8 +1,9 @@
- // Transforms Shopify items data schema to GA4 items schema
+// Transforms Shopify items data schema to GA4 items schema
 function transformVariantToItem(variant, index = 0, quantity = 1) {
   return {
-      item_id: variant.id,
-      product_id: variant.product.id,
+      item_id: variant.sku,
+      product_id: variant.product.id, // Not a default item level dimension. Need to add as a CD. 
+      variant_id: variant.id, // Not a default item level dimension. Need to add as a CD. The variant ID is broken on Ajax ATC events, and returns the line ID instead. 
       item_name: variant.product.title,
       affiliation: variant.product.vendor,
       index: index,
@@ -10,7 +11,7 @@ function transformVariantToItem(variant, index = 0, quantity = 1) {
       item_category: variant.product.type,
       item_variant: variant.title,
       price: variant.price.amount,
-      quantity: quantity
+      quantity: variant.quantity || quantity
   };
 }
 
@@ -52,18 +53,18 @@ analytics.subscribe("product_viewed", async (event) => {
   
     parent.postMessage({
       'message': 'shopify_pixel_event',
-      'event_name': view_item_list,
+      'event_name': 'view_item_list',
       'ecomData': JSON.stringify(transformedData)
     }, 
     event.context.document.location.origin);
   });
   
-  // Shopify: product_added_to_cart -> GA4: add_to_cart
-  analytics.subscribe("product_added_to_cart", async (event) => {
-    console.log('atc event: ')
-    console.log(event.data)
-    const cartLine = event.data.cartLine
-    const variant = cartLine.merchandise
+  // Shopify: product_added_to_cart & product_removed_from_cart -> GA4: add_to_cart & remove_from_cart
+  
+  function handleCartEvent(eventName, event) {
+    const cartLine = event.data.cartLine;
+    const variant = cartLine.merchandise;
+  
     function transformData(variant) {
       return {
         currency: cartLine.cost.totalAmount.currencyCode,
@@ -71,10 +72,54 @@ analytics.subscribe("product_viewed", async (event) => {
         items: [transformVariantToItem(variant, 0, cartLine.quantity)]
       };
     }
+    
     const transformedData = transformData(variant);
+  
     parent.postMessage({
       'message': 'shopify_pixel_event',
-      'event_name': 'add_to_cart',
+      'event_name': eventName,
+      'ecomData': JSON.stringify(transformedData)
+    }, 
+    event.context.document.location.origin);
+  }
+  
+  analytics.subscribe("product_added_to_cart", async (event) => {
+    handleCartEvent('add_to_cart', event);
+  });
+  
+  analytics.subscribe("product_removed_from_cart", async (event) => {
+    handleCartEvent('remove_from_cart', event);
+  });
+
+
+  // Shopify: cart_viewed -> GA4: view_cart
+  analytics.subscribe("cart_viewed", async (event) => {
+    console.log('view_cart: ', event)
+    function transformData(cartData) {
+      const cartValue = cartData.cost.totalAmount.amount
+      const cartCurrency = cartData.cost.totalAmount.currency
+      const cartLines = cartData.lines
+      let cartItems = []
+      cartLines.forEach(cartLine => {
+        let cartItem = cartLine.merchandise
+        cartItem.quantity = cartLine.quantity
+        cartItems.push(cartItem);
+      });
+      console.log('cartItems are: ', cartItems)
+      const items = cartItems.map((item, index) => transformVariantToItem(item, index));
+      console.log('items are: ', items)
+      return {
+        currency: cartCurrency,
+        value: cartValue,
+        items: items
+      };
+    }
+    const cartData = event.data.cart
+    const transformedData = transformData(cartData);
+  
+    parent.postMessage({
+      'message': 'shopify_pixel_event',
+      'event_name': 'view_cart',
       'ecomData': JSON.stringify(transformedData)
     }, 
     event.context.document.location.origin);
